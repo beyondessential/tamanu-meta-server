cf6690b1a76c49f838f72308d7a36e02cec8026a
Félix Saparelli <felix@passcod.name>
2024-09-02 23:48:54 +1200

    deps: hack up migrate to ditch libpq dependency

--- src/bin/migrate.rs
+++ src/bin/migrate.rs
@@ -1,8 +1,8 @@
 use clap::Parser;
-use diesel::{pg::PgConnection, Connection};
 use diesel_migrations::{
-	embed_migrations, EmbeddedMigrations, HarnessWithOutput, MigrationHarness,
+	embed_migrations, EmbeddedMigrations, HarnessWithOutput, MigrationHarness as _,
 };
+use rocket_db_pools::diesel::{AsyncConnection as _, AsyncPgConnection};
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
 #[derive(Debug, Parser)]
@@ -35,14 +35,25 @@ enum Mode {
 	IsOk,
 }
 
+#[cfg(feature = "migrations-with-tokio-postgres")]
+async fn connection() -> rust_postgres_migrator::UnasyncMigrator {
+	let config = tamanu_meta::db_config().unwrap();
+	let connection = AsyncPgConnection::establish(&config.url).await.unwrap();
+	rust_postgres_migrator::UnasyncMigrator { connection }
+}
+
+#[cfg(feature = "migrations-with-libpq")]
+async fn connection() -> diesel::pg::PgConnection {
+	let config = tamanu_meta::db_config().unwrap();
+	diesel::pg::PgConnection::establish(&config.url).unwrap()
+}
+
 #[rocket::main]
 async fn main() {
-	let mode = Cli::parse().mode.unwrap_or_default();
-	let config = tamanu_meta::db_config().unwrap();
-	let mut connection = PgConnection::establish(&config.url).unwrap();
+	let mut connection = connection().await;
 	let mut migrator = HarnessWithOutput::write_to_stdout(&mut connection);
 
-	match mode {
+	match Cli::parse().mode.unwrap_or_default() {
 		Mode::Run => {
 			migrator
 				.run_pending_migrations(MIGRATIONS)
@@ -93,6 +104,96 @@ async fn main() {
 			} else {
 				std::process::exit(0);
 			}
+		}
+	}
+}
+
+#[cfg(feature = "migrations-with-tokio-postgres")]
+mod rust_postgres_migrator {
+	//! to avoid needing libpq-dev or building pq-src, we hack up a migration
+	//! harness on top of `AsyncPgConnection`, just good enough for migrations.
+
+	use diesel::{
+		connection::{BoxableConnection, SimpleConnection},
+		pg::Pg,
+		sql_query, QueryResult, QueryableByName,
+	};
+	use diesel_migrations::MigrationHarness;
+	use rocket_db_pools::diesel::{
+		prelude::RunQueryDsl, AsyncPgConnection, SimpleAsyncConnection as _,
+	};
+
+	pub(crate) struct UnasyncMigrator {
+		pub(crate) connection: AsyncPgConnection,
+	}
+
+	impl SimpleConnection for UnasyncMigrator {
+		fn batch_execute(&mut self, query: &str) -> QueryResult<()> {
+			let connection = &mut self.connection;
+			std::thread::scope(|s| s.spawn(|| {
+				let runtime = rocket::tokio::runtime::Builder::new_current_thread().build().unwrap();
+				runtime.block_on(async move {
+					connection.batch_execute(query).await?;
+					Ok(())
+				})
+			}).join()).unwrap()
+		}
+	}
+
+	impl BoxableConnection<Pg> for UnasyncMigrator {
+		fn as_any(&self) -> &dyn std::any::Any {
+			todo!()
+		}
+
+		fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+			todo!()
+		}
+	}
+
+	impl MigrationHarness<Pg> for UnasyncMigrator {
+		fn run_migration(
+			&mut self,
+			migration: &dyn diesel::migration::Migration<Pg>,
+		) -> diesel::migration::Result<diesel::migration::MigrationVersion<'static>> {
+			migration.run(self)?;
+			let version = migration.name().version();
+			self.batch_execute(&format!(
+				"INSERT INTO __diesel_schema_migrations (version) VALUES ('{version}')",
+			))?;
+			Ok(version.as_owned())
+		}
+
+		fn revert_migration(
+			&mut self,
+			migration: &dyn diesel::migration::Migration<Pg>,
+		) -> diesel::migration::Result<diesel::migration::MigrationVersion<'static>> {
+			migration.revert(self)?;
+			let version = migration.name().version();
+			self.batch_execute(&format!(
+				"DELETE FROM __diesel_schema_migrations WHERE version = '{version}'",
+			))?;
+			Ok(version.as_owned())
+		}
+
+		fn applied_migrations(
+			&mut self,
+		) -> diesel::migration::Result<Vec<diesel::migration::MigrationVersion<'static>>> {
+			#[derive(QueryableByName)]
+			struct Version {
+				#[diesel(sql_type = diesel::sql_types::Text)]
+				version: String,
+			}
+
+			std::thread::scope(|s| s.spawn(|| {
+				let runtime = rocket::tokio::runtime::Builder::new_current_thread().build().unwrap();
+				runtime.block_on(async move {
+					let rows: Vec<Version> =
+						sql_query("SELECT version FROM __diesel_schema_migrations ORDER BY version DESC")
+							.get_results(&mut self.connection)
+							.await?;
+					Ok(rows.into_iter().map(|v| v.version.into()).collect())
+				})
+			}).join()).unwrap()
 		}
 	}
 }
