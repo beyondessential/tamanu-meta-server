6544306a47cc1629ef5c579525fb209adc2008c4
Félix Saparelli <felix@passcod.name>
2024-09-05 21:38:53 +1200

    feat: use ordered_servers view transparently in place of servers table

--- src/db/servers.rs
+++ src/db/servers.rs
@@ -6,7 +6,7 @@ use super::server_rank::ServerRank;
 use super::url_field::UrlField;
 
 #[derive(Debug, Clone, Serialize, Deserialize, Queryable, Selectable, Insertable)]
-#[diesel(table_name = crate::schema::servers)]
+#[diesel(table_name = crate::views::ordered_servers)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Server {
 	pub id: Uuid,
@@ -22,7 +22,7 @@ pub struct Server {
 
 impl Server {
 	pub async fn get_all(db: &mut AsyncPgConnection) -> Vec<Self> {
-		crate::schema::servers::table
+		crate::views::ordered_servers::table
 			.select(Server::as_select())
 			.load(db)
 			.await
@@ -49,7 +49,7 @@ impl From<NewServer> for Server {
 }
 
 #[derive(Debug, Deserialize, AsChangeset)]
-#[diesel(table_name = crate::schema::servers)]
+#[diesel(table_name = crate::views::ordered_servers)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct PartialServer {
 	pub id: Uuid,
