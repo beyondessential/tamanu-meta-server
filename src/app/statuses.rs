998f39b37515426b1710dac00fcc76d228a0de89
Félix Saparelli <felix@passcod.name>
2024-09-19 17:12:12 +1200

    feat: display min and max versions in active use (#6)

--- src/app/statuses.rs
+++ src/app/statuses.rs
@@ -4,17 +4,41 @@ use rocket_dyn_templates::{context, Template};
 
 use crate::{
 	app::TamanuHeaders,
-	db::{latest_statuses::LatestStatus, statuses::Status, Db},
+	db::{latest_statuses::LatestStatus, server_rank::ServerRank, statuses::Status, Db},
 };
+
+use super::versions::LiveVersionsBracket;
 
 #[get("/")]
 pub async fn view(mut db: Connection<Db>) -> TamanuHeaders<Template> {
 	let entries = LatestStatus::fetch(&mut db).await;
+
+	let mut versions = entries
+		.iter()
+		.filter_map(|status| {
+			if let (Some(version), true, ServerRank::Production) = (
+				status.latest_success_version.clone(),
+				status.is_up,
+				status.server_rank,
+			) {
+				Some(version)
+			} else {
+				None
+			}
+		})
+		.collect::<Vec<_>>();
+	versions.sort();
+	let bracket = LiveVersionsBracket {
+		min: versions.first().cloned().unwrap(),
+		max: versions.last().cloned().unwrap(),
+	};
 	TamanuHeaders::new(Template::render(
 		"statuses",
 		context! {
 			title: "Server statuses",
 			entries,
+			bracket,
+			versions,
 		},
 	))
 }
--- src/app/versions.rs
+++ src/app/versions.rs
@@ -1,13 +1,35 @@
 use rocket::serde::{json::Json, Serialize};
+use rocket_db_pools::Connection;
 
-use crate::app::TamanuHeaders;
+use crate::{
+	app::{TamanuHeaders, Version},
+	db::{latest_statuses::LatestStatus, server_rank::ServerRank},
+	Db,
+};
 
 #[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize)]
-pub struct VersionResponse {
-	version: node_semver::Version,
+pub struct LiveVersionsBracket {
+	pub min: Version,
+	pub max: Version,
 }
 
-#[get("/version/<version>")]
-pub fn view(version: &str) -> TamanuHeaders<Json<VersionResponse>> {
-	todo!("resolve {version}")
+#[get("/versions")]
+pub async fn view(mut db: Connection<Db>) -> TamanuHeaders<Json<LiveVersionsBracket>> {
+	let statuses = LatestStatus::only_up(&mut db).await;
+	let mut versions = statuses
+		.iter()
+		.filter_map(|status| {
+			if let (Some(version), ServerRank::Production) =
+				(status.latest_success_version.clone(), status.server_rank)
+			{
+				Some(version)
+			} else {
+				None
+			}
+		})
+		.collect::<Vec<_>>();
+	versions.sort();
+	let min = versions.first().cloned().expect("no versions returned");
+	let max = versions.last().cloned().expect("no versions returned");
+	TamanuHeaders::new(LiveVersionsBracket { min, max }.into())
 }
