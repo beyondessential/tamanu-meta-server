f6201ae5d1f5f4ee4dc8386425e802c2c976b070
Félix Saparelli <felix@passcod.name>
2025-03-18 12:15:42 +1300

    wip

--- src/app/version.rs
+++ src/app/version.rs
@@ -1,5 +1,6 @@
 use diesel::{backend::Backend, deserialize, expression::AsExpression, serialize, sql_types::Text};
-use rocket::{http::Header, serde::Serialize};
+use node_semver::SemverError;
+use rocket::{http::Header, request::FromParam, serde::Serialize};
 
 #[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, AsExpression)]
 #[diesel(sql_type = Text)]
@@ -14,6 +15,14 @@ impl Default for Version {
 impl From<Version> for Header<'_> {
 	fn from(version: Version) -> Self {
 		Header::new("X-Tamanu-Version", version.0.to_string())
+	}
+}
+
+impl FromParam<'_> for Version {
+	type Error = SemverError;
+
+	fn from_param(param: &'_ str) -> Result<Self, Self::Error> {
+		param.parse().map(Self)
 	}
 }
 
--- src/app/versions.rs
+++ src/app/versions.rs
@@ -1,10 +1,17 @@
 use rocket::serde::json::Json;
 use rocket_db_pools::Connection;
 
-use crate::{app::TamanuHeaders, db::versions::Version, Db};
+use crate::{app::{TamanuHeaders, Version as ParsedVersion}, db::versions::Version, Db};
 
 #[get("/versions")]
 pub async fn view(mut db: Connection<Db>) -> TamanuHeaders<Json<Vec<Version>>> {
+	let list_of_versions = Version::get_all(&mut db).await;
+	TamanuHeaders::new(list_of_versions.into())
+}
+
+#[get("/versions/update-for/<version>")]
+pub async fn update_for(mut db: Connection<Db>, version: ParsedVersion) -> TamanuHeaders<Json<Vec<Version>>> {
+	let major = version.0.major;
 	let list_of_versions = Version::get_all(&mut db).await;
 	TamanuHeaders::new(list_of_versions.into())
 }
