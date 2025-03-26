c061ebcd8e063e9494ee1d9ccb6dbcd1e51666d3
Daniel Nash <dannash200@gmail.com>
2025-03-27 12:01:12 +1300

    closer

--- src/db/servers.rs
+++ src/db/servers.rs
@@ -14,7 +14,9 @@ pub struct Server {
 	pub id: Uuid,
 
 	// name and host are required for the public API
+	#[diesel(deserialize_as = String, serialize_as = String)]
 	pub name: Option<String>,
+
 	#[diesel(deserialize_as = String, serialize_as = String)]
 	pub host: UrlField,
 
