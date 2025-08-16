aded23d827c49597ffaf44b3dfca9ec4bde17a0e
Félix Saparelli <felix@passcod.name>
2025-08-16 17:33:37 +1200

    cleanup debug artifact

--- migrations/2025-08-16-050804_drop_error_field_from_statuses/up.sql
+++ migrations/2025-08-16-050804_drop_error_field_from_statuses/up.sql
@@ -4,6 +4,7 @@ DROP INDEX IF EXISTS latest_statuses_successes;
 
 -- Drop the latest_statuses view since it was designed around error/success distinction
 DROP VIEW IF EXISTS latest_statuses;
+DROP VIEW IF EXISTS latest_statuses_dbg;
 
 -- Drop the error column from statuses table
 ALTER TABLE statuses DROP COLUMN error;
