a567b2f9c0b2d23603b48a0a517ff46d60415e77
Félix Saparelli <felix@passcod.name>
2025-10-02 19:17:29 +1300

    fix merge

--- crates/database/src/schema.rs
+++ crates/database/src/schema.rs
@@ -11,69 +11,80 @@ pub mod sql_types {
 }
 
 diesel::table! {
-    admins (email) {
-        email -> Text,
-        created_at -> Timestamptz,
-    }
+	admins (email) {
+		email -> Text,
+		created_at -> Timestamptz,
+	}
 }
 
 diesel::table! {
-    artifacts (id) {
-        id -> Uuid,
-        created_at -> Timestamptz,
-        updated_at -> Timestamptz,
-        version_id -> Uuid,
-        artifact_type -> Text,
-        platform -> Text,
-        download_url -> Text,
-    }
+	artifacts (id) {
+		id -> Uuid,
+		created_at -> Timestamptz,
+		updated_at -> Timestamptz,
+		version_id -> Uuid,
+		artifact_type -> Text,
+		platform -> Text,
+		download_url -> Text,
+	}
 }
 
 diesel::table! {
-    device_connections (id) {
-        id -> Uuid,
-        created_at -> Timestamptz,
-        device_id -> Uuid,
-        ip -> Inet,
-        user_agent -> Nullable<Text>,
-    }
+	device_connections (id) {
+		id -> Uuid,
+		created_at -> Timestamptz,
+		device_id -> Uuid,
+		ip -> Inet,
+		user_agent -> Nullable<Text>,
+	}
 }
 
 diesel::table! {
-    device_keys (id) {
-        id -> Uuid,
-        created_at -> Timestamptz,
-        updated_at -> Timestamptz,
-        device_id -> Uuid,
-        key_data -> Bytea,
-        name -> Nullable<Text>,
-        is_active -> Bool,
-    }
+	device_keys (id) {
+		id -> Uuid,
+		created_at -> Timestamptz,
+		updated_at -> Timestamptz,
+		device_id -> Uuid,
+		key_data -> Bytea,
+		name -> Nullable<Text>,
+		is_active -> Bool,
+	}
 }
 
 diesel::table! {
-    use diesel::sql_types::*;
-    use super::sql_types::DeviceRole;
+	use diesel::sql_types::*;
+	use super::sql_types::DeviceRole;
 
-    devices (id) {
-        id -> Uuid,
-        created_at -> Timestamptz,
-        updated_at -> Timestamptz,
-        role -> DeviceRole,
-    }
+	devices (id) {
+		id -> Uuid,
+		created_at -> Timestamptz,
+		updated_at -> Timestamptz,
+		role -> DeviceRole,
+	}
 }
 
 diesel::table! {
-    servers (id) {
-        id -> Uuid,
-        created_at -> Timestamptz,
-        updated_at -> Timestamptz,
-        name -> Nullable<Text>,
-        rank -> Nullable<Text>,
-        host -> Text,
-        device_id -> Nullable<Uuid>,
-        kind -> Text,
-    }
+	servers (id) {
+		id -> Uuid,
+		created_at -> Timestamptz,
+		updated_at -> Timestamptz,
+		name -> Nullable<Text>,
+		rank -> Nullable<Text>,
+		host -> Text,
+		device_id -> Nullable<Uuid>,
+		kind -> Text,
+	}
+}
+
+diesel::table! {
+	statuses (id) {
+		id -> Uuid,
+		created_at -> Timestamptz,
+		server_id -> Uuid,
+		version -> Nullable<Text>,
+		extra -> Jsonb,
+		device_id -> Nullable<Uuid>,
+	}
 }
 
 diesel::table! {
@@ -100,12 +111,12 @@ diesel::joinable!(statuses -> devices (device_id));
 diesel::joinable!(statuses -> servers (server_id));
 
 diesel::allow_tables_to_appear_in_same_query!(
-    admins,
-    artifacts,
-    device_connections,
-    device_keys,
-    devices,
-    servers,
-    statuses,
-    versions,
+	admins,
+	artifacts,
+	device_connections,
+	device_keys,
+	devices,
+	servers,
+	statuses,
+	versions,
 );
